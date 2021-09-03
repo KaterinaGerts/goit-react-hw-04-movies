f4e270efe941dd7b219dada190098fe4cea669be
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-09-03 13:47:44 +0300

    создала папки для компонентов и сделала фетч

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import movieApi from 'services/movie-api';
+
+function App() {
+
+  movieApi()
+
+  return (
+    <div >
+   123
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/movie-api.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://api.themoviedb.org/3/trending/all/day?';
+const KEY = '5f8f89b44874dae418398127927dd3d2';
+
+export default function movieApi() {
+  return fetch(`${BASE_URL}api_key=${KEY}`,)
+    .then(response => {    
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(data => {
+      console.log(data.results);
+      return data.results;
+    });
+}
+
+
